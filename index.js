435b20a8ff6453fdc84eb16efa7c3a4ed2d292e2
Camilo Sanchez <camilin87@gmail.com>
2016-07-20 16:35:44 -0400

    create a beta that logs rate limits

--- index.js
+++ index.js
@@ -28,5 +28,39 @@ module.exports = function(accessToken, accessTokenSecret, callBackUrl,
         config.callBackUrl = callBackUrl;
     }
 
-    return new Twitter(config);
+    var result = new Twitter(config);
+
+    var prevGet = result.oauth.get;
+    result.oauth.get = function(url, accessToken, accessTokenSecret, callback){
+        return prevGet(url, accessToken, accessTokenSecret, (err, body, response) => {
+            console.log("TwitterClient.Get",
+                "X-Rate-Limit-Limit",
+                response.headers["x-rate-limit-limit"],
+                "X-Rate-Limit-Remaining",
+                response.headers["x-rate-limit-remaining"],
+                "X-Rate-Limit-Reset",
+                response.headers["x-rate-limit-reset"]
+            );
+
+            callback(err, body, response);
+        });
+    };
+
+    var prevPost = result.oauth.post;
+    result.oauth.post = function(url, accessToken, accessTokenSecret, post_body, contentType, callback){
+        return prevPost(url, accessToken, accessTokenSecret, post_body, contentType, (err, body, response) => {
+            console.log("TwitterClient.Post",
+                "X-Rate-Limit-Limit",
+                response.headers["x-rate-limit-limit"],
+                "X-Rate-Limit-Remaining",
+                response.headers["x-rate-limit-remaining"],
+                "X-Rate-Limit-Reset",
+                response.headers["x-rate-limit-reset"]
+            );
+
+            callback(err, body, response);
+        });
+    };
+
+    return result;
 }
